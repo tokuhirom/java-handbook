13d43f13bfc7835565dfe822957adad9d1ac3eee
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-09-20 23:10:45 +0900

    remove comment

--- src/test/java/com/example/collection/map/ImmutableMapTest.java
+++ src/test/java/com/example/collection/map/ImmutableMapTest.java
@@ -3,9 +3,6 @@ package com.example.collection.map;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
-/**
- * Created by tokuhirom on 9/17/16.
- */
 public class ImmutableMapTest {
     @Test
     public void foo() {
@@ -13,6 +10,5 @@ public class ImmutableMapTest {
                 .put("foo", "bar")
                 .put("hoge", "fuga")
                 .build();
-
     }
 }
