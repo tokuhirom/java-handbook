7a265075fe65505afa72cc09ad6d311410a1726a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-08-19 16:22:51 +0900

    move collections to includeBuild

---
+++ collection/collections/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:33.3.0-jre")
+    testImplementation("org.assertj:assertj-core:3.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
