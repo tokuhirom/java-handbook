1a10fbf02a7df92b9903f7c7dee64f9f0cf777e1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-08-19 18:11:40 +0900

    move mockito module

--- testing/mockito/build.gradle.kts
+++ testing/mockito/build.gradle.kts
@@ -2,12 +2,20 @@ plugins {
     id("java")
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
-    implementation("com.google.guava:guava")
+    implementation("com.google.guava:guava:33.3.0-jre")
 
     testImplementation("org.mockito:mockito-core:4.11.0")
     testImplementation("org.mockito:mockito-junit-jupiter:4.11.0")
-    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
